a627160549c5be6e9fe521c11e784b265db979f1
ingan <isabelngan27@gmail.com>
2019-06-22 21:33:38 -0700

    updated location teak and adjusted image and stat - working in console.log

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -13,72 +13,72 @@ var chef = [
     {
         "name": "Niki Nakayama",
         "restaurant": "n/naka",
-        "location": "Los Angeles, CA, USA",
-        "image": "./assets/images/n/naka.jpg",
+        "donde": "Los Angeles, CA, USA",
+        "image": "./assets/images/nnaka.jpg",
         "hint": "She attended culinary school in Pasadena, after which she worked at Mori Sushie is partners with her Sous Chef"
     },
     {
         "name": "Alex Atala",
         "restaurant": "D.O.M",
-        "location": "São Paulo, Brazil",
-        "image": "./assets/images/D.O.M.jpg",
+        "donde": "São Paulo, Brazil",
+        "image": "./assets/images/DOM.jpg",
         "hint": "He's known for transforming traditional Brazilian dishes, adapting French and Italian culinary techniques to native Brazilian ingredients."
     },
     {
         "name": "Jeong Kwan",
         "restaurant": "She doesn't own a restaurant!",
-        "location": " Baegyangsa Temple, South Korean",
+        "donde": " Baegyangsa Temple, South Korean",
         "image": "./assets/images/jeongkwan.jpg",
         "hint": "A monk with no traditional culinary training."
     },
     {
         "name": "Virgilio Martinez Veliz",
         "restaurant": "Central",
-        "location": "Lima, Peru",
+        "donde": "Lima, Peru",
         "image": "./assets/images/central.jpg",
         "hint": "He is known for his use of unique ingredients such as a potato grown at 5,000 meters above sea level."
     },
     {
         "name": "Nancy Silverton",
         "restaurant": "MOZZA",
-        "location": "Los Angeles, CA, USA",
+        "donde": "Los Angeles, CA, USA",
         "image": "./assets/images/mozza.jpg",
         "hint": "She created LaBrea Bakery."
     },
     {
         "name": "Grant Achatz",
         "restaurant": "Alinea",
-        "location": "Chicago, IL, USA",
+        "donde": "Chicago, IL, USA",
         "image": "./assets/images/alinea.jpg",
         "hint": "He was diagnosed with stage 4 squamous cell carcinoma of the mouth."
     },
     {
         "name": "Asma Khan",
         "restaurant": "Darjeeling Express",
-        "location": "London, United Kingdom",
+        "donde": "London, United Kingdom",
         "image": "./assets/images/asmadarjeeling.jpg",
         "hint": "She created her restaurant with chefs that had no culinary background, but rather a love for food."
     },
     {
         "name": "Bo Songvisava",
         "restaurant": "Bo.Lan",
-        "location": "Bangkok, Thailand",
+        "donde": "Bangkok, Thailand",
         "image": "./assets/images/Bo.Lan.jpg",
         "hint": "She makes real Thai cuisine without the any western infleunce."
     },
     {
         "name": "Francis Mallmann",
         "restaurant": "Patagonia Sur",
-        "location": "Buenos Aires, Argentina",
+        "donde": "Buenos Aires, Argentina",
         "image": "./assets/images/PatagoniaSur.jpg",
         "hint": "He focuses on various Patagonian methods of barbecuing food."
     },
     {
         "name": "Albert Adria",
         "restaurant": "Tickets",
-        "location": "Barcelona, Spain",
+        "donde": "Barcelona, Spain",
         "image": "./assets/images/Tickets.jpg",
-        "hint": "He ramained in this older bother's shadow until her created his restaurant, Tickets."
+        "hint": "His had created over 1.800 dishes for his former restaurant, El Bulli"
     },
 ]
 
@@ -112,9 +112,9 @@ var chefpic = document.getElementById('chefimage');
 
 var chefstatT = document.getElementById('chefstat');
 
-var locationT = document.getElementById('location');
+var donde;
 
-var restaurantT = document.getElementById('restaurant');
+var restaurant;
 
 var resultT = document.getElementById('result');
 
@@ -152,12 +152,22 @@ function selectchef() {
     currentchef = randomchef;
     // pulling name
     chefname = currentchef.name;
+    console.log(chefname);
     // set hint = 
     hintlink = currentchef.hint;
-    console.log(chefname);
     // set image = 
     chefface = currentchef.image;
+    // restaurant
+    restaurant = currentchef.restaurant;
+    console.log(restaurant)
+    // location
+    donde = currentchef.donde
+    console.log(donde)
 };
+
+// setting up location and Restuarant
+document.getElementById('donde').innerHTML = donde;
+document.getElementById('restaurant').innerHTML = restaurant;
 
 //Initialize the GAME 
 
@@ -182,13 +192,14 @@ function initializeGame() {
     resultT.textContent = "";
     // hint for chef
     document.getElementById('hint').innerHTML = hintlink;
+    // next button
+    nextbutton.style.display = "none";
 }
 
 //convert to underscore
 function underscorechange() {
     var displayunderscore = chefname.toLowerCase().replace(/[a-z]/g, "_")
     return displayunderscore
-    console.log("change to underscore");
 };
 
 
@@ -199,45 +210,69 @@ var guessreset = function () {
     usedletters = [];
 }
 
-// Show Stats
-function showdeets() {
-    locationT.textContent = currentchef.location;
-    restaurantT.textContent = currentchef.restaurant;
-}
+    // Updated status  of Wins thus far
 
 
-// Updated status  of Wins thus far
+    // delete word from array
 
 
-// delete word from array
+    // Actually Playing the games
 
-//next button
-nextbutton.onclick = function () {
-    nextbutton.style.display = "none";
-    initializeGame();
-}
+    function playTurn(play) {
+        guess = play.key;
+        guess = guess.toLowerCase();
+        // limiting to just letters pressed
+        if (parseInt(guess) || guess === "0") {
+            return;
+        }
 
-// Actually Playing the games
+        // Guess history
+        if (usedletters.indexOf(guess) >= 0) {
+            return;
+            // this makes that repeating number not affect current changes
+        } else {
+            //this pushes the players incorrect guess to the usedArray and writes it to the HTML
+            usedletters.push(guess);
+            document.getElementById('guess').innerHTML = usedletters;
+        }
 
-function playTurn(play) {
-    var userplay = play.key;
-    userplay = userplay.toLowerCase();
-    // limiting to just letters pressed
-    if (parseInt(userplay) || userplay === "0") {
-        return;
+        // User Guess match Letter ======
+        // splitting up words into letters
+        var wordletter = chefname.split("");
+
+        if (guess === wordletter) {
+            remaining--;
+            selectedletter();
+            console.log(displayword)
+        }
+
+        // win
+        if (displayword === chefname) {
+            win++;
+            gamewins++;
+            wordsremain--;
+            resultT.textContent = "You Got it!";
+            chefpic.style.display = "block";
+            chefstatT.style.display = "block";
+            nextbutton.style.display = "block";
+
+        }
+        // lose
+        if (remiaining === 0) {
+            wordsremain--;
+            displaychef.textContent = chefname;
+            resultT.textContent = "Oops! Better luck next time.";
+            chefpic.style.display = "block";
+            chefstatT.style.display = "block";
+            nextbutton.style.display = "block";
+        }
+
+
+
     }
 
-    // Guess history
-    if (usedletters.indexOf(guess) >= 0) {
-        return;
-        // this makes that repeating number not affect current changes
-    } else {
-        //this pushes the players incorrect guess to the usedArray and writes it to the HTML
-        usedletters.push(guess);
-        document.getElementById('guess').innerHTML = usedletters;
+    //next button
+    nextbutton.onclick = function () {
+        nextbutton.style.display = "none";
+        initializeGame();
     }
-
-    // replacing letters
-
-
-}
